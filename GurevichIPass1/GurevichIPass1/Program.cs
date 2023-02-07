04194734418891d4f2d48b0ca75a50ca733ea554
Idan Gurevich <60413202+IdanG7@users.noreply.github.com>
2023-02-07 16:02:41 -0500

    Add files via upload

---
+++ GurevichIPass1/GurevichIPass1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+namespace GurevichIPass1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Game wordleGame = new Game();
+            wordleGame.WordleGame();
+        }
+    }
+
+    class Game
+    {
+        static Random rnd = new Random();
+
+        public void WordleGame()
+        {
+            string wordleAnswers = File.ReadAllText("WordleAnswers.txt");
+            string[] answersArray = wordleAnswers.Split('\n');
+
+            int randNum = rnd.Next(0, answersArray.Length);
+            string randWord = answersArray[randNum];
+
+            CreateMainScreen();
+
+
+
+
+            static void CreateMainScreen()
+            {
+                Console.WriteLine("──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
+                Console.WriteLine("─██████──────────██████─██████████████─████████████████───██████─────────████████████───██████─────────██████████████─");
+                Console.WriteLine("─██░░██──────────██░░██─██░░░░░░░░░░██─██░░░░░░░░░░░░██───██░░██─────────██░░░░░░░░████─██░░██─────────██░░░░░░░░░░██─");
+                Console.WriteLine("─██░░██──────────██░░██─██░░██████░░██─██░░████████░░██───██░░██─────────██░░████░░░░██─██░░██─────────██░░██████████─");
+                Console.WriteLine("─██░░██──██████──██░░██─██░░██──██░░██─██░░████████░░██───██░░██─────────██░░██──██░░██─██░░██─────────██░░██████████─");
+                Console.WriteLine("─██░░██──██░░██──██░░██─██░░██──██░░██─██░░░░░░░░░░░░██───██░░██─────────██░░██──██░░██─██░░██─────────██░░░░░░░░░░██─");
+                Console.WriteLine("─██░░██──██░░██──██░░██─██░░██──██░░██─██░░██████░░████───██░░██─────────██░░██──██░░██─██░░██─────────██░░██████████─");
+                Console.WriteLine("─██░░██████░░██████░░██─██░░██──██░░██─██░░██──██░░██─────██░░██─────────██░░██──██░░██─██░░██─────────██░░██─────────");
+                Console.WriteLine("─██░░░░░░░░░░░░░░░░░░██─██░░██████░░██─██░░██──██░░██████─██░░██████████─██░░████░░░░██─██░░██████████─██░░██████████─");
+                Console.WriteLine("─██░░██████░░██████░░██─██░░░░░░░░░░██─██░░██──██░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░████─██░░░░░░░░░░██─██░░░░░░░░░░██─");
+                Console.WriteLine("─██████──██████──██████─██████████████─██████──██████████─██████████████─████████████───██████████████─██████████████─");
+                Console.WriteLine("──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
+
+                Console.WriteLine("\n\n\n\t\t\t\t\t|-------|-------|-------|-------|-------|");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|-------|-------|-------|-------|-------|");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|-------|-------|-------|-------|-------|");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|-------|-------|-------|-------|-------|");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|-------|-------|-------|-------|-------|");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|-------|-------|-------|-------|-------|");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|       |       |       |       |       |");
+                Console.WriteLine("\t\t\t\t\t|-------|-------|-------|-------|-------|");
+
+
+            }
+
+
+
+        }
+    }
+}
